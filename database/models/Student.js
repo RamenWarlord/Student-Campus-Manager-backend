0e549681bcdffc1a51b75f3e2ba58532468ae2c2
ramenwarlord <ivanyeung12345@gmail.com>
2021-12-19 04:43:26 -0500

    copied over starter code, added config to gitignore

---
+++ database/models/Student.js
@@ -0,0 +1,16 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const Student = db.define("student", {
+  firstname: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+
+  lastname: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Student;
